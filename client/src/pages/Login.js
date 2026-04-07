13ba9a7385a7c344a12d9908de586ccf6857b21b
charan171020 <kbcharan17102005@gmail.com>
2026-04-08 00:26:54 +0530

    Fixed API endpoint for login and updated backend URL

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -16,7 +16,7 @@ function Login() {
 
     try {
       const res = await axios.post(
-        "https://task-manager-l102.onrender.com",
+        "https://task-manager-1102.onrender.com/api/auth/login",
         { email, password }
       );
 
